3875c01a162c5ba31b002f3d1b5088ebf5290a4a
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2019-12-20 14:25:32 -0600

    Keybind for (goto-line)

--- lib/keybindings.el
+++ lib/keybindings.el
@@ -36,6 +36,7 @@
 (global-set-key (kbd "TAB") 'indent-appropriately)
 (global-set-key (kbd "C-x C-v") 'clipboard-yank)
 (global-set-key (kbd "C-^") 'enlarge-window)
+(global-set-key (kbd "C-c g l") 'goto-line)
 
 ;; Kill this buffer!
 (substitute-key-definition 'kill-buffer 'kill-buffer-and-window global-map)
