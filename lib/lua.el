1df3eaec6e6ff6f35e51259632463a4d4ddcd99d
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2020-05-14 17:38:41 -0500

    lsp support for lua

--- lib/lua.el
+++ lib/lua.el
@@ -11,14 +11,23 @@
 ;; http://immerrr.github.io/lua-mode/
 ;; TODO: require a `luacheck' install
 (use-package lua-mode
-  :defer t
   :straight t
-  :defer
-  (add-hook 'lua-mode-hook 'lsp-deferred))
+  :init
+  (add-hook 'lua-mode-hook 'lsp-deferred)
+  (setq lua-indent-string-contents t))
 
 ;; Use lua-mode for PICO-8 source files
 ;; TODO: document that this is here, or make a separate pico8.el file.
 (setq auto-mode-alist (append '(("\\.p8$" . lua-mode))
                               auto-mode-alist))
+
+;; https://github.com/EmmyLua/EmmyLua-LanguageServer/blob/58ac70f40d84cf7ce0e31fd3aba2f021d9d18c72/readme.md#adding-to-emacs
+;; https://github.com/phenix3443/lsp-lua-emmy
+(use-package lsp-lua-emmy
+  :demand
+  :straight (lsp-lua-emmy :type git :host github :repo "phenix3443/lsp-lua-emmy")
+  :init
+  (setq lsp-lua-emmy-jar-path
+        (expand-file-name "EmmyLua-LS-all-ac977d4.jar" (concat (getenv "HOME") "/src/EmmyLua-LanguageServer"))))
 
 ;;; lua.el ends here
