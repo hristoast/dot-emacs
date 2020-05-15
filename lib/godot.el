a7623a9a3d40200081de1c6af6f5d0f5476ae876
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2020-05-14 21:06:28 -0500

    Format on save with gdtoolkit

--- lib/godot.el
+++ lib/godot.el
@@ -4,6 +4,10 @@
 ;;; Code:
 
 ;; Major mode for editing Godot GDScript files
+;; https://github.com/GDQuest/emacs-gdscript-mode
+;; https://github.com/scony/godot-gdscript-toolkit/
+;; python3 -m pip install --user gdtoolkit
+;; C-c C-r C-s is bound to (gdscript-godot-run-current-scene)!
 (use-package gdscript-mode
   :straight (gdscript-mode
              :type git
@@ -11,6 +15,11 @@
              :repo "GDQuest/emacs-gdscript-mode")
   ;; Use toml-mode for .godot and .tscn source files
   :init
+  (defun gdscript-format-buffer-save-hook ()
+    (interactive)
+    (when (eq major-mode 'gdscript-mode)
+      (gdscript-format-buffer)))
+  (add-hook 'before-save-hook 'gdscript-format-buffer-save-hook)
   (add-hook 'gdscript-mode-hook 'lsp-deferred)
   (setq auto-mode-alist (append '(("\\.godot$" . toml-mode))
                                 '(("\\.tscn$" . toml-mode))
