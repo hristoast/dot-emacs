4007d4c8f651f9eba3378b5f513f32e7cc5f52ba
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-02-07 14:07:43 -0600

    Default to eglot over lsp-mode, update a lot of things

--- lib/h-lsp.el
+++ lib/h-lsp.el
@@ -3,18 +3,31 @@
 ;; Packages related to lsp-mode
 ;;; Code:
 
-;; Emacs client/library for the Language Server Protocol
-;; https://github.com/emacs-lsp/lsp-mode
-(use-package lsp-mode
-  :straight t
-  :init
-  (setq
-   lsp-clients-python-library-directories
-   (concat (getenv "HOME") ".local/lib/python3.8/site-packages"))
-  :config
-  ;; Disable python "features" that are useless to me
-  (setq lsp-pyls-plugins-mccabe-enabled nil
-        lsp-pyls-plugins-pycodestyle-enabled nil))
+(if (getenv "EMACS_LSP_MODE")
+    ;; Emacs client/library for the Language Server Protocol
+    ;; https://github.com/emacs-lsp/lsp-mode
+    (use-package lsp-mode
+      :straight t
+      :init
+      (setq
+       lsp-clients-python-library-directories
+       (concat (getenv "HOME") ".local/lib/python3.8/site-packages"))
+      :config
+      ;; Disable python "features" that are useless to me
+      (setq lsp-pyls-plugins-mccabe-enabled nil
+            lsp-pyls-plugins-pycodestyle-enabled nil))
+
+  ;; Emacs Polyglot: an Emacs LSP client that stays out of your way:
+  ;; https://github.com/joaotavora/eglot
+  (use-package eglot :straight t
+    :init
+    (setq
+     lsp-clients-python-library-directories
+     (concat (getenv "HOME") ".local/lib/python3.8/site-packages"))
+    :config
+    ;; Disable python "features" that are useless to me
+    (setq lsp-pyls-plugins-mccabe-enabled nil
+          lsp-pyls-plugins-pycodestyle-enabled nil)))
 
 (use-package lsp-ui :straight t)
 
