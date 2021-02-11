312839b3cf7c3346a940c76407385009935ff336
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-02-10 19:33:52 -0600

    Clean up
    I'm not actually using or missing markdown-mode+ even though it has
    some nifty features.

--- lib/markdown.el
+++ lib/markdown.el
@@ -5,22 +5,10 @@
 
 ;; Emacs Markdown Mode
 ;; https://github.com/jrblevin/markdown-mode
-(use-package markdown-mode
-  :straight t
-  :config
-  ;; Special indent for markdown-mode
-  ;; TODO: set this the right way
-  ;; (add-hook 'markdown-mode-hook
-  ;;           (global-set-key (kbd "TAB") 'md-indent))
-  (defun md-indent ()
-    "Indent for `markdown-mode', to be used to rebind TAB - WIP."
-    (interactive)
-    (if mark-active
-        (do-func-to-marked-region 'markdown-indent-region)
-      (markdown-indent-line))))
+(use-package markdown-mode :straight t :defer t)
 
 ;; Additional functions for Emacs [markdown-mode].
 ;; https://github.com/milkypostman/markdown-mode-plus
-(use-package markdown-mode+ :straight t :defer t)
+;; (use-package markdown-mode+ :straight t :defer t)
 
 ;;; markdown.el ends here
