d46c4097a3f7534929a29cb19e347b10049ccb7e
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-03-09 17:37:44 -0600

    Use early-init

---
+++ early-init.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- Early loading!
+;;; Commentary:
+;;
+;; Things should that happen early on go here.
+;;
+;;; Code:
+
+;; This is a job for straight.el
+(setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
