a05eae5a57e4ca28d5bfeb092122bef32fe3dfe5
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-03-20 09:59:54 -0500

    No more eglot
    I don't use it so there's no sense trying to support it.

--- lib/golang.el
+++ lib/golang.el
@@ -16,18 +16,10 @@
   :init
   (defun h/golang-before-save ()
     "Do the right thing whether it's lsp-mode or eglot."
-    (if (getenv "EMACS_LSP_MODE")
-        ;; lsp-mode
         (progn
           (lsp-format-buffer)
-          (lsp-organize-imports))
-
-      ;; eglot
-      (progn
-        ;; TODO: This is apparently wrong
-        ;; Error: (wrong-type-argument commandp eglot-format-buffer)
-        ;; (call-interactively 'eglot-format-buffer)
-        (call-interactively 'eglot-code-action-organize-imports))))
-  :hook (before-save . h/golang-before-save))
+          (lsp-organize-imports)))
+  :hook
+  (before-save . h/golang-before-save))
 
 ;;; golang.el ends here
--- lib/h-lsp.el
+++ lib/h-lsp.el
@@ -3,74 +3,44 @@
 ;; Packages related to lsp-mode
 ;;; Code:
 
-(if (getenv "EMACS_EGLOT")
-  ;; Emacs Polyglot: an Emacs LSP client that stays out of your way:
-  ;; https://github.com/joaotavora/eglot
-  (use-package eglot :straight t
-    ;; Hook into modes that I always want LSP functionality in.
-    :hook
-    ((gdscript-mode . eglot-ensure)
-     (go-mode . eglot-ensure)
-     (web-mode . eglot-ensure)
-     (c-mode . eglot-ensure)
-     (c++-mode . eglot-ensure)
-     (js-mode . eglot-ensure)
-     (json-mode . eglot-ensure)
-     (python-mode . eglot-ensure)
-     (ruby-mode . eglot-ensure)
-     (sh-mode . eglot-ensure))
-    :init
-    (setq eglot-workspace-configuration
-          '((pyls
-             (plugins
-              (mccabe
-               (enabled . nil))
-              (pycodestyle
-               (enabled . nil))
-              (pydocstyle
-               (enabled . t))
-              (jedi_completion
-               (fuzzy . t)
-               (follow_builtin_imports . :json-false)))))))
 
-  (progn
-      ;; Emacs client/library for the Language Server Protocol
-      ;; https://github.com/emacs-lsp/lsp-mode
-      (use-package lsp-mode
-        :straight t
-        :hook
-        ((gdscript-mode . lsp-deferred)
-         (go-mode . (lambda ()
-                      (progn
-                        (lsp-deferred)
-                        (lsp-register-custom-settings
-                         '(("gopls.completeUnimported" t t)
-                           ("gopls.staticcheck" t t))))))
-         (c-mode . lsp-deferred)
-         (c++-mode . lsp-deferred)
-         (web-mode . lsp-deferred)
-         (js-mode . lsp-deferred)
-         (json-mode . lsp-deferred)
-         (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred)))
-         (ruby-mode . lsp-deferred)
-         (sh-mode . lsp-deferred))
-        :init
-        ;; Knob to disable the headerline breadcrum stuff
-        (if (getenv "EMACS_LSP_NO_HEADERLINE_BREADCRUMB")
-            (lsp-headerline-breadcrumb-mode 0))
-        (setq
-         lsp-prefer-flymake nil
-         lsp-clients-python-library-directories
-         (concat (getenv "HOME") ".local/lib/python3.8/site-packages"))
-        :config
-        ;; Disable python "features" that are useless to me
-        (setq lsp-pyls-plugins-mccabe-enabled nil
-              lsp-pyls-plugins-pycodestyle-enabled nil))
+;; Emacs client/library for the Language Server Protocol
+;; https://github.com/emacs-lsp/lsp-mode
+(use-package lsp-mode
+  :straight t
+  :hook
+  ((gdscript-mode . lsp-deferred)
+   (go-mode . (lambda ()
+                (progn
+                  (lsp-deferred)
+                  (lsp-register-custom-settings
+                   '(("gopls.completeUnimported" t t)
+                     ("gopls.staticcheck" t t))))))
+   (c-mode . lsp-deferred)
+   (c++-mode . lsp-deferred)
+   (web-mode . lsp-deferred)
+   (js-mode . lsp-deferred)
+   (json-mode . lsp-deferred)
+   (python-mode . (lambda ()
+                    (require 'lsp-pyright)
+                    (lsp-deferred)))
+   (ruby-mode . lsp-deferred)
+   (sh-mode . lsp-deferred))
+  :init
+  ;; Knob to disable the headerline breadcrum stuff
+  (if (getenv "EMACS_LSP_NO_HEADERLINE_BREADCRUMB")
+      (lsp-headerline-breadcrumb-mode 0))
+  (setq
+   lsp-prefer-flymake nil
+   lsp-clients-python-library-directories
+   (concat (getenv "HOME") ".local/lib/python3.8/site-packages"))
+  :config
+  ;; Disable python "features" that are useless to me
+  (setq lsp-pyls-plugins-mccabe-enabled nil
+        lsp-pyls-plugins-pycodestyle-enabled nil))
 
-      ;; UI integrations for lsp-mode
-      ;; https://emacs-lsp.github.io/lsp-ui/
-      (use-package lsp-ui :straight t)))
+;; UI integrations for lsp-mode
+;; https://emacs-lsp.github.io/lsp-ui/
+(use-package lsp-ui :straight t)
 
 ;;; h-lsp.el ends here
