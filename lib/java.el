d5fe935e7c851d4d0579c5817766a07ec539acb2
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-08-22 11:21:13 -0500

    This is obsolete because lsp-mode

--- lib/java.el
+++ lib/java.el
@@ -8,13 +8,13 @@
 
 ;; A Better Java Development Environment for Emacs
 ;; https://github.com/mopemope/meghanada-emacs
-(use-package meghanada
-  :straight t
-  :hook (java-mode . (lambda ()
-                       (setq-default meghanada-mode t)
-                       (meghanada-server-start)
-                       ;; TODO: 10 below is arbitrary, the server takes
-                       ;; around 6 seconds to start on my primary workstation
-                       (run-with-timer 10 nil 'meghanada-client-direct-connect))))
+;; (use-package meghanada
+;;   :straight t
+;;   :hook (java-mode . (lambda ()
+;;                        (setq-default meghanada-mode t)
+;;                        (meghanada-server-start)
+;;                        ;; TODO: 10 below is arbitrary, the server takes
+;;                        ;; around 6 seconds to start on my primary workstation
+;;                        (run-with-timer 10 nil 'meghanada-client-direct-connect))))
 
 ;;; java.el ends here
