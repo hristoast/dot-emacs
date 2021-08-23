8052776ed9a16c35f7bb77125a4bee9e2d365a5e
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-08-23 17:57:16 -0500

    This isn't needed with lsp-mode

--- lib/c-cpp.el
+++ lib/c-cpp.el
@@ -6,11 +6,6 @@
 ;;TODO: document system packages that are required
 ;;TODO: bear, clangd
 
-;; Clang format
-;; http://clang.llvm.org/docs/ClangFormat.html
-;; http://clang.llvm.org/docs/ClangFormatStyleOptions.html
-(use-package clang-format :straight t :defer t)
-
 ;; major-mode for editing CMake sources
 ;; https://github.com/Kitware/CMake/blob/master/Auxiliary/cmake-mode.el
 (use-package cmake-mode :defer t :straight t)
