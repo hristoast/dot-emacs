212d5989ae95aaa501f0da983037a391b3aff616
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-08-23 18:06:20 -0500

    Gimme 4 spaces for C++, please

--- lib/c-cpp.el
+++ lib/c-cpp.el
@@ -6,6 +6,8 @@
 ;;TODO: document system packages that are required
 ;;TODO: bear, clangd
 
+(use-package c++-mode :straight nil :init (setq c-basic-offset 4))
+
 ;; major-mode for editing CMake sources
 ;; https://github.com/Kitware/CMake/blob/master/Auxiliary/cmake-mode.el
 (use-package cmake-mode :defer t :straight t)
