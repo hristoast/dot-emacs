9c9e40c9630a74b2ad02be3ec07b0123e5962f66
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-08-23 17:57:31 -0500

    Actually remove those

--- lib/editing.el
+++ lib/editing.el
@@ -14,42 +14,6 @@
   (electric-indent-mode -1)
   (setq clean-aindent-is-simple-indent t))
 
-;; Modular in-buffer completion framework for Emacs
-;; http://company-mode.github.io/
-(use-package company
-  :straight t
-  :diminish company-mode
-  :hook (after-init . global-company-mode)
-  :config
-  (setq
-   company-echo-delay 0
-   company-idle-delay 0.0
-   company-minimum-prefix-length 1
-   company-tooltip-align-annotations t
-   company-tooltip-limit 20)
-  ;; Default colors are awful - borrowed these from gocode (thanks!):
-  ;; https://github.com/nsf/gocode/tree/master/emacs-company#color-customization
-  ;; See: M-x list-colors-display
-  ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Colors.html
-  (set-face-attribute
-   'company-preview nil :foreground "black" :underline t)
-  (set-face-attribute
-   'company-preview-common nil :inherit 'company-preview)
-  (set-face-attribute
-   'company-tooltip nil :background "gray44" :foreground "black")
-  (set-face-attribute
-   'company-tooltip-selection nil :background "LavenderBlush4" :foreground "DarkOliveGreen2")
-  (set-face-attribute
-   'company-tooltip-common nil :foreground "OliveDrab3" :weight 'bold)
-  (set-face-attribute
-   'company-tooltip-common-selection nil :foreground "black" :weight 'bold))
-
-(unless (getenv "EMACS_NO_COMPANY_ICONS")
-  (use-package company-box
-    :diminish company-box-mode
-    :straight t
-    :hook (company-mode . company-box-mode)))
-
 (unless (getenv "EMACS_NO_DIMMER")
   ;; Interactively highlight which buffer is active by dimming the others.
   ;; https://github.com/gonewest818/dimmer.el
@@ -71,15 +35,6 @@
   :config
   (setq global-mode-string (remove 'dtrt-indent-mode-line-info global-mode-string))
   (dtrt-indent-mode 1))
-
-;; Syntax checking for GNU Emacs - http://www.flycheck.org/
-(use-package flycheck
-  :straight t
-  :bind
-  (("C-c e n" . flycheck-next-error)
-   ("C-c e p" . flycheck-previous-error))
-  ;; https://www.flycheck.org/en/latest/user/installation.html#use-package
-  :init (global-flycheck-mode))
 
 ;; Ignore "vendor" directories when rgrepping. Useful for some go projects I've worked on.
 (use-package grep
@@ -202,7 +157,7 @@
 ;; Emacs package that displays available keybindings in popup
 ;; https://github.com/justbur/emacs-which-key/
 (use-package which-key
-  :diminish which-key
+  :diminish which-key-mode
   :straight t
   :config (which-key-mode))
 
