7ec253a90961b1576a672609f19f60170d62abef
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-12-30 08:13:33 -0600

    Update gdscript-mode and its usage
    Cleaning up old funcs and things, less is now needed to get it doing
    what I need it to do.

--- lib/godot.el
+++ lib/godot.el
@@ -6,39 +6,28 @@
 ;; Major mode for editing Godot GDScript files
 ;; https://github.com/GDQuest/emacs-gdscript-mode
 ;; https://github.com/scony/godot-gdscript-toolkit/
-;; python3 -m pip install --user gdtoolkit
+;; python3 -m pip install gdtoolkit
 ;; C-c C-r C-s is bound to (gdscript-godot-run-current-scene)!
 (use-package gdscript-mode
   :straight (gdscript-mode
              :type git
              :host github
-             :repo "GDQuest/emacs-gdscript-mode")
+             :repo "godotengine/emacs-gdscript-mode")
   :hook
-  (before-save . hristoast-gdscript-format-buffer-save-hook)
+  (before-save . (lambda ()
+                   (when (eq major-mode 'gdscript-mode)
+                     (progn
+                       (gdscript-format-buffer)))))
   :init
-  (bind-key "C-x r s" 'hristoast-godot-run-current-scene-debug)
-
-  (defun hristoast-godot-run-current-scene-debug ()
-    "Run the current scene in debug mode and switch to the output buffer."
-    (interactive)
-    (progn
-      (let ((godot-buffer "*godot*"))
-        (if (get-buffer godot-buffer)
-            (with-current-buffer godot-buffer (erase-buffer)))
-        (gdscript-godot-run-current-scene-debug)
-        (switch-to-buffer-other-window godot-buffer))))
-
-  (defun hristoast-gdscript-format-buffer-save-hook ()
-    (when (eq major-mode 'gdscript-mode)
-      (progn
-        (gdscript-format-buffer)
-        ;; Gotta run this to get things going again...
-        (lsp))))
-
-  (setq auto-mode-alist (append '(("\\.godot$" . conf-toml-mode))
-                                '(("\\.tres$" . conf-toml-mode))
-                                '(("\\.tscn$" . conf-toml-mode))
-                                '(("\\.gd$" . gdscript-mode))
-                                auto-mode-alist)))
+  (setq-default gdscript-docs-local-path "~/.local/godot-engine/docs")
+  (bind-key "C-x r s" (lambda ()
+                        "Run the current scene in debug mode and switch to the output buffer."
+                        (interactive)
+                        (progn
+                          (let ((godot-buffer "*godot*"))
+                            (if (get-buffer godot-buffer)
+                                (with-current-buffer godot-buffer (erase-buffer)))
+                            (gdscript-godot-run-current-scene-debug)
+                            (switch-to-buffer-other-window godot-buffer))))))
 
 ;;; godot.el ends here
