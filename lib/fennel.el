af2a135ed598b633423d990db442c267b3c1ebaa
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2022-04-19 07:06:51 -0500

    Compile that Fennel

--- lib/fennel.el
+++ lib/fennel.el
@@ -6,6 +6,12 @@
 ;; https://gitlab.com/technomancy/fennel-mode/-/blob/686e4d28a8abeb1fa05cb21e14c4f0cc12217d63/Readme.md
 (use-package fennel-mode
   :straight t
+  :hook
+  (after-save . (lambda ()
+                  (when (eq major-mode 'fennel-mode)
+                    (let ((compilation-read-command nil)
+                          (compile-command "make"))
+                      (call-interactively 'compile)))))
   :init
   (add-to-list 'auto-mode-alist '("\\.fnl\\'" . fennel-mode)))
 
