cd5d582d71a647c4f4524dae8819661cf9b2d616
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2022-05-17 19:33:22 -0500

    OMWFX time

--- lib/shaders.el
+++ lib/shaders.el
@@ -7,4 +7,7 @@
 ;; https://github.com/jimhourihan/glsl-mode
 (use-package glsl-mode :defer t :straight t)
 
+;; Open omwfx as GLSL
+(add-to-list 'auto-mode-alist '("\\.omwfx$" . glsl-mode))
+
 ;;; shaders.el ends here
