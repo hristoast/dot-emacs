34ac027f58a89a8892303edaccbca85cd6a4a04e
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2023-08-30 18:25:15 -0500

    Document and add one more

--- lib/h-lsp.el
+++ lib/h-lsp.el
@@ -9,24 +9,37 @@
   :defer t
   :config
   (add-to-list 'eglot-server-programs
+               '(nginx-mode . ("nginx-language-server")))
+  (add-to-list 'eglot-server-programs
                '(terraform-mode . ("terraform-ls" "serve")))
   (add-to-list 'eglot-server-programs
                '(toml-mode . ("taplo" "lsp" "stdio")))
   :hook
+  ;; https://emacs-lsp.github.io/lsp-mode/page/lsp-ansible/
   ((ansible-mode . eglot-ensure)
+   ;; system package: ccls
    (c-mode . eglot-ensure)
    (c++-mode . eglot-ensure)
    ;; https://github.com/rcjsuen/dockerfile-language-server-nodejs
    (dockerfile-mode . eglot-ensure)
+   ;; system package: gopls
    (go-mode . eglot-ensure)
    ;; https://github.com/typescript-language-server/typescript-language-server
    (js-mode . eglot-ensure)
+   ;; https://www.npmjs.com/package/vscode-json-languageserver
    (json-mode . eglot-ensure)
+   ;; system package: lua-language-server
    (lua-mode . eglot-ensure)
+   ;; https://github.com/pappasam/nginx-language-server
    (nginx-mode . eglot-ensure)
+   ;; system package: python3-lsp-server
    (python-mode . eglot-ensure)
+   ;; https://github.com/Shopify/ruby-lsp
+   ;; https://github.com/castwide/solargraph
    (ruby-mode . eglot-ensure)
+   ;; system package: bash-language-server
    (sh-mode . eglot-ensure)
+   ;; system package: terraform-ls
    (terraform-mode . eglot-ensure)
    ;; https://github.com/tamasfe/taplo/releases/tag/0.8.1
    (toml-mode . eglot-ensure)
