c7eaf3395832ab1fe6c1e8b617caec10b172e7b1
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2024-03-23 14:01:59 -0500

    Do a fmt prior to saving

--- lib/terraform.el
+++ lib/terraform.el
@@ -6,6 +6,10 @@
 
 ;; Major mode of Terraform configuration file
 ;; https://github.com/syohex/emacs-terraform-mode
-(use-package terraform-mode :straight t :defer t)
+(use-package terraform-mode
+  :straight t
+  :defer t
+  :hook
+  (terraform-mode . terraform-format-on-save-mode))
 
 ;;; terraform.el ends here
