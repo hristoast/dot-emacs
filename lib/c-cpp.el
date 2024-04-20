dc6df430214b70ccdd492216fa116699af597b72
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2024-04-20 08:04:49 -0500

    clang-format on save, please

--- lib/c-cpp.el
+++ lib/c-cpp.el
@@ -6,7 +6,11 @@
 (use-package cc-mode
   ;; :ensure-system-package (clang (bear . Bear))
   :straight nil
+  :hook
+  (before-save . clang-format-buffer)
   :init (setq c-basic-offset 4))
+
+(use-package clang-format :defer t :straight t)
 
 ;; major-mode for editing CMake sources
 ;; https://github.com/Kitware/CMake/blob/master/Auxiliary/cmake-mode.el
