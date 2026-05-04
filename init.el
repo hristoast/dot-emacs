591022d57a7a704a4812a4eff876a3a80eaa7856
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2026-05-04 15:16:40 -0500

    That's what it's called now

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
       "EMACS_NO_CLOJURE" "clojure"
       "EMACS_NO_CSS" "css"
       "EMACS_NO_DOCKER" "docker"
-      "EMACS_NO_ESHELL" "h-eshell"
+      "EMACS_NO_SHELL" "h-shell"
       "EMACS_NO_EXTRAS" "h-extras"
       "EMACS_NO_FENNEL" "fennel"
       "EMACS_NO_FISH" "fish"
