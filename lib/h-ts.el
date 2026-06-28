26236fa73d6edec173b875d6c33f655bfd8ba238
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2026-06-27 20:57:10 -0500

    That too

--- lib/h-ts.el
+++ lib/h-ts.el
@@ -1,22 +1,11 @@
-
-;; (use-package tree-sitter
-;;   :straight t
-;;   :hook
-;;   (eglot . (lambda ()
-;;                  (progn
-;;                    (tree-sitter-hl-mode -1)
-;;                    (message "IDK JAJA"))))
-;;   ;;TODO: need to selectively enable this; it looks _TERRIBLE_ for YAML
-;;   ;; (tree-sitter-after-on . (lambda ()
-;;   ;;                           (tree-sitter-hl-mode -1)))
-;;   ;; (lua-mode . tree-sitter-hl-mode)
-;;   ;; (python-mode . tree-sitter-hl-mode)
-;;   ;; (go-mode . (lambda () (tree-sitter-hl-mode 0)))
-;;   )
-
-;; (use-package tree-sitter-langs :straight t)
+;;; h-ts.el --- eglot and related
+;;; Commentary:
+;; Packages related to eglot
+;;; Code:
 
 (when (treesit-available-p)
   (use-package treesit-auto
     :straight t
     :config (global-treesit-auto-mode)))
+
+;;; h-ts.el ends here
